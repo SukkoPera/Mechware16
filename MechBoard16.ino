b09a061b088dafbc000dff04758e63b6a5de75f1
SukkoPera <software@sukkology.net>
2024-01-08 23:36:29 +0100

    Code cleanup

--- MechBoard16.ino
+++ MechBoard16.ino
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * This file is part of MechBoard16.                                           *
+ *                                                                             *
+ * Copyright (C) 2024 by SukkoPera <software@sukkology.net>                    *
+ *                                                                             *
+ * MechBoard16 is free software: you can redistribute it and/or                *
+ * modify it under the terms of the GNU General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * MechBoard16 is distributed in the hope that it will be useful,              *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with MechBoard16. If not, see http://www.gnu.org/licenses.            *
+ ******************************************************************************/
+
+// Send debug messages to serial port
+//~ #define ENABLE_SERIAL_DEBUG
+
 const byte PIN_LED_R = A5;
 const byte PIN_LED_G = A4;
 const byte PIN_LED_B = A3;
@@ -13,16 +35,13 @@ const byte MATRIX_ROWS = 8;
  */
 const byte MATRIX_COLS = 8;
 
-//~ const byte PIN_ROWS[MATRIX_ROWS] = {17 /* RXLED */, 15 /* SCK */, 16 /* MOSI */, 14 /* MISO */, 8, 9, 10, 11};
-//~ const byte PIN_COLS[MATRIX_COLS] = {3, 2, 0, 1, 4, 30 /* TXLED */, 12, 6};
-
 #include "CircularBuffer.h"
-struct MatrixSample {
+struct KeyMatrixSample {
 	uint8_t rows;
 	uint8_t cols;
 };
 
-/* volatile */ CircularBuffer<MatrixSample, uint8_t, 32> matrixSamples;
+/* volatile */ CircularBuffer<KeyMatrixSample, uint8_t, 32> matrixSamples;
 
 #include "LedControl.h"
 LedControl lc(PIN_MAX7221_DATA, PIN_MAX7221_CLK, PIN_MAX7221_SEL, 1 /* Number of MAX72xx chips */);
@@ -116,6 +135,27 @@ enum class Mode: uint8_t {
 
 Mode mode = Mode::PRESSED_OFF;
 
+/*******************************************************************************
+ * END OF SETTINGS
+ ******************************************************************************/
+
+#ifdef ENABLE_SERIAL_DEBUG
+	//~ #include <avr/pgmspace.h>
+	//~ typedef const __FlashStringHelper * FlashStr;
+	//~ typedef const byte* PGM_BYTES_P;
+	//~ #define PSTR_TO_F(s) reinterpret_cast<const __FlashStringHelper *> (s)
+
+	#define dstart(spd) Serial.begin (spd)
+	#define debug(...) Serial.print (__VA_ARGS__)
+	#define debugln(...) Serial.println (__VA_ARGS__)
+#else
+	#define dstart(...)
+	#define debug(...)
+	#define debugln(...)
+#endif
+
+
+// TODO: Move all the following tables to flash memory
 
 // Order in which they appear on the keyboard (+1 for 2 Shifts)
 constexpr Key splash_order[N_KEYS + 1] = {
@@ -128,15 +168,6 @@ constexpr Key splash_order[N_KEYS + 1] = {
 };
 
 constexpr Key keymap[MATRIX_ROWS][MATRIX_COLS] = {
-	//~ {Key::DEL,   Key::_3,    Key::_5, Key::_7, Key::_9, Key::DOWN,   Key::LEFT,      Key::_1},
-	//~ {Key::ENTER, Key::W,     Key::R,  Key::Y,  Key::I,  Key::P,      Key::ASTERISK,  Key::CLEAR},
-	//~ {Key::POUND, Key::A,     Key::D,  Key::G,  Key::J,  Key::L,      Key::SEMICOLON, Key::CTRL},
-	//~ {Key::HELP,  Key::_4,    Key::_6, Key::_8, Key::_0, Key::UP,     Key::RIGHT,     Key::_2},
-	//~ {Key::F1,    Key::Z,     Key::C,  Key::B,  Key::M,  Key::PERIOD, Key::ESC,       Key::SPACE},
-	//~ {Key::F2,    Key::S,     Key::F,  Key::H,  Key::K,  Key::COLON,  Key::EQUAL,     Key::CMD},
-	//~ {Key::F3,    Key::E,     Key::T,  Key::U,  Key::O,  Key::MINUS,  Key::PLUS,      Key::Q},
-	//~ {Key::AT,    Key::SHIFT, Key::X,  Key::V,  Key::N,  Key::COMMA,  Key::SLASH,     Key::RUNSTOP}
-
 	{Key::DEL,  Key::ENTER,    Key::POUND,     Key::HELP,  Key::F1,     Key::F2,    Key::F3,    Key::AT},
 	{Key::_3,   Key::W,        Key::A,         Key::_4,    Key::Z,      Key::S,     Key::E,     Key::SHIFT},
 	{Key::_5,   Key::R,        Key::D,         Key::_6,    Key::C,      Key::F,     Key::T,     Key::X},
@@ -147,18 +178,61 @@ constexpr Key keymap[MATRIX_ROWS][MATRIX_COLS] = {
 	{Key::_1,   Key::CLEAR,    Key::CTRL,      Key::_2,    Key::SPACE,  Key::CMD,   Key::Q,     Key::RUNSTOP}
 };
 
-#define LED_ROW(mtxR, mtxC) (mtxC)
-#define LED_COL(mtxR, mtxC) ((mtxR + 1) % 8)
+const char KEY_NAMES[MATRIX_ROWS][MATRIX_COLS][4] = {
+	{"DEL", "RET", "£",   "HLP", "F1",  "F2", "F3", "@"},
+	{"3",   "W",   "A",   "4",   "Z",   "S",  "E",  "SHF"},
+	{"5",   "R",   "D",   "6",   "C",   "F",  "T",  "X"},
+	{"7",   "Y",   "G",   "8",   "B",   "H",  "U",  "V"},
+	{"9",   "I",   "J",   "0",   "M",   "K",  "O",  "N"},
+	{"DN",  "P",   "L",   "UP",  ".",   ":",  "-",  ","},
+	{"LF",  "*",   ";",   "RT",  "ESC", "=",  "+",  "/"},
+	{"1",   "CLR", "CTL", "2",   "SPC", "C=", "Q",  "RUN"}
+};
 
-struct Position {
+// Current state of the keyboard matris, true if a key is pressed
+boolean matrix[MATRIX_ROWS][MATRIX_COLS] = {false};
+
+struct LedCoordinates {
 	byte row;
 	byte col;
 };
 
-Position positions[N_KEYS];
+// Maps a key to the (row, col) tuple that controls its led.
+LedCoordinates ledCoordinates[N_KEYS];
 
+// Populates the ledCoordinates array
+boolean buildLedCoordinates () {
+	bool found = false;
+	
+	for (byte i = 0; i < N_KEYS; ++i) {
+		Key k = static_cast<Key> (i);
+
+		found = false;
+		for (byte row = 0; row < MATRIX_ROWS && !found; ++row) {
+			for (byte col = 0; col < MATRIX_COLS && !found; ++col) {
+				if (keymap[row][col] == k) {
+					/* The led matrix was supposed to be the same as the keyboard matrix. I don't know whether I made a
+					 * wiring mistake or if LedControl numbers things differently, but it turns out we need to swap the
+					 * coordinates and modify them slightly in order to use them with lc.setLed().
+					 */
+					ledCoordinates[i].row = col;
+					ledCoordinates[i].col = (row + 1) % 8;
+					found = true;
+				}
+			}
+		}
+
+		if (!found) {
+			break;
+		}
+	}
+
+	return found;
+}
+
+// Pin-change ISR, called whenever the C16 polls the keyboard, all we do is snoop what is going on ;)
 ISR (PCINT0_vect) {
-	MatrixSample ms {
+	KeyMatrixSample ms {
 		.rows = PINB,
 		.cols = PIND
 	};
@@ -186,14 +260,21 @@ unsigned int countSetBits (int n) {
 	return count; 
 }
 
+// Called when a keypress is detected
 void onKeyPressed (const byte row, const byte col) {
 	switch (mode) {
-		case Mode::PRESSED_ON:
-			lc.setLed(0, LED_ROW (row, col), LED_COL (row, col), true);
+		case Mode::PRESSED_ON: {
+			const Key k = keymap[row][col];
+			const LedCoordinates& pos = ledCoordinates[static_cast<int> (k)];
+			lc.setLed(0, pos.row, pos.col, true);
 			break;
-		case Mode::PRESSED_OFF:
-			lc.setLed(0, LED_ROW (row, col), LED_COL (row, col), false);
+		}
+		case Mode::PRESSED_OFF: {
+			const Key k = keymap[row][col];
+			const LedCoordinates& pos = ledCoordinates[static_cast<int> (k)];
+			lc.setLed(0, pos.row, pos.col, false);
 			break;
+		}
 		case Mode::ALWAYS_ON:
 		case Mode::ALWAYS_OFF:
 			// Nothing to do
@@ -201,14 +282,21 @@ void onKeyPressed (const byte row, const byte col) {
 	}
 }
 
+// Called when a keyrelease is detected
 void onKeyReleased (const byte row, const byte col) {
 	switch (mode) {
-		case Mode::PRESSED_ON:
-			lc.setLed(0, LED_ROW (row, col), LED_COL (row, col), false);
+		case Mode::PRESSED_ON: {
+			const Key k = keymap[row][col];
+			const LedCoordinates& pos = ledCoordinates[static_cast<int> (k)];
+			lc.setLed(0, pos.row, pos.col, false);
 			break;
-		case Mode::PRESSED_OFF:
-			lc.setLed(0, LED_ROW (row, col), LED_COL (row, col), true);
+		}
+		case Mode::PRESSED_OFF: {
+			const Key k = keymap[row][col];
+			const LedCoordinates& pos = ledCoordinates[static_cast<int> (k)];
+			lc.setLed(0, pos.row, pos.col, true);
 			break;
+		}
 		case Mode::ALWAYS_ON:
 		case Mode::ALWAYS_OFF:
 			// Nothing to do
@@ -216,70 +304,49 @@ void onKeyReleased (const byte row, const byte col) {
 	}
 }
 
-void setup() {
-	/*
-	 The MAX72XX is in power-saving mode on startup,
-	 we have to do a wakeup call
-	 */
+void setup () {
+	dstart (115200);
+	
+	// Wake up and configure the MAX72XX ASAP, since it might show a random pattern at startup
 	lc.shutdown (0, false);
-	/* Set the brightness to a medium values */
-	lc.setIntensity (0, 15);
-	/* and clear the display */
+	lc.setIntensity (0, 15);		// 0-15
 	lc.clearDisplay (0);
 
-	for (byte i = 0; i < N_KEYS; ++i) {
-		Key k = static_cast<Key> (i);
-
-		bool found = false;
-		byte row, col;
-		for (row = 0; row < MATRIX_ROWS && !found; ++row) {
-			for (col = 0; col < MATRIX_COLS && !found; ++col) {
-				if (keymap[row][col] == k) {
-					// Not sure why, but we need to swap the coordinates and modify them slightly
-					positions[i].row = LED_ROW (row, col);
-					positions[i].col = LED_COL (row, col);
-					found = true;
-				}
-			}
-		}
-
-		if (!found) {
-			while (true) {
-			}
-		}
-	}
-
-	// Keyboard polling pins
-
-	// TED drives the rows, which we have on PORT B
-	//~ for (byte i = 0; i < MATRIX_ROWS; ++i) {
-		//~ pinMode (PIN_ROWS[i], INPUT);
-	//~ }
-
-	// Enable pin-change interrupts on all pins of PORT B
+	/* Keyboard polling: TED drives the rows, which we have on PORT B, while the keyboard "outputs" the columns, which
+	 * we have on PORT D. They are all INPUTs by default se all we have to do is to enable the pin-change interrupts on
+	 * all pins of PORT B.
+	 */
 	PCMSK0 = (1 << PCINT7) | (1 << PCINT6) | (1 << PCINT5) | (1 << PCINT4) |
 	         (1 << PCINT3) | (1 << PCINT2) | (1 << PCINT1) | (1 << PCINT0);
 	PCICR |= (1 << PCIE0);
 
-	// The keyboard "outputs" the columns, which we have on PORT D
-	//~ for (byte i = 0; i < MATRIX_COLS; ++i) {
-		//~ pinMode (PIN_COLS[i], INPUT);
-	//~ }
-
-
-	// R/G/B LED pins
+	// R/G/B LED pins: configure as OUTPUTs
 	pinMode (PIN_LED_R, OUTPUT);
 	pinMode (PIN_LED_G, OUTPUT);
 	pinMode (PIN_LED_B, OUTPUT);
 
+	// Build the LED coordinates array
+	if (!buildLedCoordinates()) {
+		debugln (F("Unable to build the LED coordinates array, this indicatess a mistake in the code"));
 
-	for (byte i = 0; i < N_KEYS + 1; ++i) {
-		const Position& pos = positions[static_cast<int> (splash_order[i])];
-		lc.setLed(0, pos.row, pos.col, true);
-		delay(DELAY_TIME);
-		lc.setLed(0, pos.row, pos.col, false);
+		// Hang with fast blinking
+		while (true) {
+			digitalWrite (PIN_LED_R, HIGH);
+			delay (222);
+			digitalWrite (PIN_LED_R, LOW);
+			delay (222);
+		}
 	}
 
+	// Do the power-up animation
+	for (byte i = 0; i < N_KEYS + 1; ++i) {
+		const LedCoordinates& pos = ledCoordinates[static_cast<int> (splash_order[i])];
+		lc.setLed (0, pos.row, pos.col, true);
+		delay (DELAY_TIME);
+		lc.setLed (0, pos.row, pos.col, false);
+	}
+
+	// Prepare the initial LED pattern according to the chosen mode
 	switch (mode) {
 		case Mode::ALWAYS_ON:
 		case Mode::PRESSED_OFF:
@@ -293,38 +360,23 @@ void setup() {
 			// Nothing to do, leds are already all off at this point
 			break;
 	}
-
-	Serial.begin(115200);
 }
-
-boolean matrix[MATRIX_ROWS][MATRIX_COLS] = {false};
-
-const char KEY_NAMES[MATRIX_ROWS][MATRIX_COLS][4] = {
-	{"DEL", "RET", "£",   "HLP", "F1",  "F2", "F3", "@"},
-	{"3",   "W",   "A",   "4",   "Z",   "S",  "E",  "SHF"},
-	{"5",   "R",   "D",   "6",   "C",   "F",  "T",  "X"},
-	{"7",   "Y",   "G",   "8",   "B",   "H",  "U",  "V"},
-	{"9",   "I",   "J",   "0",   "M",   "K",  "O",  "N"},
-	{"DN",  "P",   "L",   "UP",  ".",   ":",  "-",  ","},
-	{"LF",  "*",   ";",   "RT",  "ESC", "=",  "+",  "/"},
-	{"1",   "CLR", "CTL", "2",   "SPC", "C=", "Q",  "RUN"}
-};
 
 void loop () {
 	while (matrixSamples.available ()) {
-		MatrixSample sample = matrixSamples.get ();
+		KeyMatrixSample sample = matrixSamples.get ();
 
 		if (sample.rows == 0x00 && sample.cols == 0xFF) {
 			// All keys released
 			for (byte r = 0; r < MATRIX_ROWS; ++r) {
 				for (byte c = 0; c < MATRIX_COLS; ++c) {
 					if (matrix[r][c]) {
-						Serial.print ("Released ");
-						Serial.print (r);
-						Serial.print (',');
-						Serial.print (c);
-						Serial.print (": ");
-						Serial.println (KEY_NAMES[r][c]);
+						debug ("Released ");
+						debug (r);
+						debug (',');
+						debug (c);
+						debug (": ");
+						debugln (KEY_NAMES[r][c]);
 
 						onKeyReleased (r, c);
 
@@ -339,21 +391,21 @@ void loop () {
 					for (byte c = 0; c < MATRIX_COLS; ++c) {
 						boolean pressed = (sample.cols & (1 << c)) == 0;
 						if (pressed && !matrix[r][c]) {
-							Serial.print ("Pressed ");
-							Serial.print (r);
-							Serial.print (',');
-							Serial.print (c);
-							Serial.print (": ");
-							Serial.println (KEY_NAMES[r][c]);
+							debug ("Pressed ");
+							debug (r);
+							debug (',');
+							debug (c);
+							debug (": ");
+							debugln (KEY_NAMES[r][c]);
 
 							onKeyPressed (r, c);
 						} else if (!pressed && matrix[r][c]) {
-							Serial.print ("Released ");
-							Serial.print (r);
-							Serial.print (',');
-							Serial.print (c);
-							Serial.print (": ");
-							Serial.println (KEY_NAMES[r][c]);
+							debug ("Released ");
+							debug (r);
+							debug (',');
+							debug (c);
+							debug (": ");
+							debugln (KEY_NAMES[r][c]);
 
 							onKeyReleased (r, c);
 						}
